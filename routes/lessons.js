d445d259b3decd3460c70dfedb9b89cb63e0a707
Jessica Leach <jessicamaryleach@gmail.com>
2017-03-27 11:32:00 +0200

    lesson pug files render

--- routes/lessons.js
+++ routes/lessons.js
@@ -6,23 +6,23 @@ var express = require('express'),
 
 //to see admin panel, users must be logged in
 
-var requireUser = (req, res, next) => {
-  if (req.path === '/') {
-    return next();
-  }
-
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect('/admin');
-  }
-};
-
-router.use(requireUser);
+// var requireUser = (req, res, next) => {
+//   if (req.path === '/') {
+//     return next();
+//   }
+//
+//   if (req.session.user) {
+//     next();
+//   } else {
+//     res.redirect('/admin');
+//   }
+// };
+//
+// router.use(requireUser);
 
 //redirect unlogged-in users to log in pages
 
-router.get('/', (req, res) => {
+router.get('/lessons', (req, res) => {
   if (req.session.user) {
     res.redirect('/admin/posts');
   }
@@ -31,10 +31,33 @@ router.get('/', (req, res) => {
 });
 
 
-//gets home page of logged-in admin panel
 router.get('/eye', (req, res) => {
   db.Post.findAll({ order: [['createdAt', 'DESC']] }).then((blogPosts) => {
     res.render('lessons/eye', { blogPosts: blogPosts, user: req.session.user });
+  }).catch((error) => {
+    throw error;
+  });
+});
+
+router.get('/diffusion', (req, res) => {
+  db.Post.findAll({ order: [['createdAt', 'DESC']] }).then((blogPosts) => {
+    res.render('lessons/diffusion', { blogPosts: blogPosts, user: req.session.user });
+  }).catch((error) => {
+    throw error;
+  });
+});
+
+router.get('/sound_waves', (req, res) => {
+  db.Post.findAll({ order: [['createdAt', 'DESC']] }).then((blogPosts) => {
+    res.render('lessons/sound_waves', { blogPosts: blogPosts, user: req.session.user });
+  }).catch((error) => {
+    throw error;
+  });
+});
+
+router.get('/nitrogen', (req, res) => {
+  db.Post.findAll({ order: [['createdAt', 'DESC']] }).then((blogPosts) => {
+    res.render('lessons/nitrogen', { blogPosts: blogPosts, user: req.session.user });
   }).catch((error) => {
     throw error;
   });

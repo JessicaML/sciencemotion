a189c924a8a7a9bb6c46bea1fe0c294a2a8cf79f
Jessica Leach <jessicamaryleach@gmail.com>
2017-04-16 14:19:17 +0100

    db working again

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "sciencemotion",
+    "database": "blog-app",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "sciencemotion-test",
+    "database": "blog-app-test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "sciencemotion-production",
+    "database": "blog-app-production",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }

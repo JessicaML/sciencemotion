2ac8bc1b08541cd7628ecb816b2f7342965bc299
Jessica Leach <jessicamaryleach@gmail.com>
2017-05-07 08:57:18 +0100

    Revert "db working again"
    This reverts commit a189c924a8a7a9bb6c46bea1fe0c294a2a8cf79f.
    Try previous db

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "blog-app",
+    "database": "sciencemotion",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "blog-app-test",
+    "database": "sciencemotion-test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": null,
-    "database": "blog-app-production",
+    "database": "sciencemotion-production",
     "host": "127.0.0.1",
     "dialect": "postgres"
   }

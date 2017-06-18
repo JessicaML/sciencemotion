3471aa53fa4130cedcd3aff855500aa500c86caf
Jessica Leach <jessicamaryleach@gmail.com>
2017-06-18 16:50:17 +0100

    remove local host IP from config file

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": null,
     "database": "sciencemotion",
-    "host": "127.0.0.1",
+    "host": "",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": null,
     "database": "sciencemotion-test",
-    "host": "127.0.0.1",
+    "host": "",
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": null,
     "database": "sciencemotion-production",
-    "host": "127.0.0.1",
+    "host": "",
     "dialect": "postgres"
   }
 };
